9b6ba3a35efdc07cc9044d6dca3f952e4f369ce9
SimoAlbi28 (Simone Albini) <simone.albini28@gmail.com>
2025-11-25 10:30:07 +0100

    feat img sistemate

--- src/components/card-components/RecipeImage.tsx
+++ src/components/card-components/RecipeImage.tsx
@@ -9,11 +9,11 @@ const RecipeImage = ({ image, title }: RecipeImageProps) => {
     : "https://images.unsplash.com/photo-1547385203-cfe7977b9fd0?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1171"
 
   return (
-    <div className="w-full h-50 rounded-2xl overflow-hidden shadow-lg relative bg-gray-100 flex items-center justify-center mb-2">
+    <div className="w-full rounded-2xl overflow-hidden shadow-lg relative bg-gray-100 flex items-center justify-center mb-2">
       <img
         src={srcValue}
         alt={title || "Titolo non disponibile"}
-        className="object-cover w-full h-full"
+        className="object-contain w-full max-h-56"
       />
       <div className="absolute inset-0" />
     </div>
